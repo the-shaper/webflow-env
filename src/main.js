1e9c4eae05a413857241dd4bbf351faa984ca1fd
the-shaper <96361405+the-shaper@users.noreply.github.com>
2024-09-06 11:56:25 -0700

    added pagination

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+//import Pikaday from 'pikaday'
 import initializeCalculator from './features/calculator'
 import initializeFormLogic from './features/formLogic'
 import initializeRadioButtons from './features/radiobutts'
+import './styles/style.css'
+import 'pikaday/css/pikaday.css'
 
 const calculator = initializeCalculator()
 initializeFormLogic(calculator)
