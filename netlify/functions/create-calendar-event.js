bf1009c24da60137ff188b418ddd5183514576a2
the-shaper <info@alejandroalarcon.ink>
2025-02-08 18:51:47 -0800

    testing matzus cal 1st connection

--- netlify/functions/create-calendar-event.js
+++ netlify/functions/create-calendar-event.js
@@ -3,7 +3,7 @@ const { google } = require('googleapis')
 exports.handler = async (event) => {
   // Add CORS headers
   const headers = {
-    'Access-Control-Allow-Origin': 'https://multi-step-form-c8f42a.webflow.io',
+    'Access-Control-Allow-Origin': 'https://okokay.webflow.io',
     'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Allow-Methods': 'POST, OPTIONS',
   }
